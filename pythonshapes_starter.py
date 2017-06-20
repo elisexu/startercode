bc8d7c2355d2a620b4f0acf466259714c6b7d4e0
Elise Xu <elisexu98@gmail.com>
2017-06-20 12:42:03 -0700

    created shape starter code

---
+++ pythonshapes_starter.py
@@ -0,0 +1,21 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = 0
+y_pos = 0
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+
+
+
+
+
+
+# Close window on click.
+exitonclick()
